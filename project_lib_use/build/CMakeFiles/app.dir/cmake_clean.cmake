5b7864ed43d173ba2a56f8d2f51071e5343486bb
iWHT <459255319@qq.com>
2024-05-06 22:43:13 +0800

    cmake

---
+++ project_lib_use/build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/lilithgames/Desktop/workspace/cmake/project_lib_use/bin/app"
+  "/Users/lilithgames/Desktop/workspace/cmake/project_lib_use/bin/app.pdb"
+  "CMakeFiles/app.dir/src/main.cpp.o"
+  "CMakeFiles/app.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
