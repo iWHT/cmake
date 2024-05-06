5be63523d901ca78d541e00b42842561a9be3c9a
iWHT <459255319@qq.com>
2024-05-06 21:41:01 +0800

    cmake

---
+++ project_lib/build/CMakeFiles/prog.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/lilithgames/Desktop/workspace/cmake/project_lib/src/add.cpp" "CMakeFiles/prog.dir/src/add.cpp.o" "gcc" "CMakeFiles/prog.dir/src/add.cpp.o.d"
+  "/Users/lilithgames/Desktop/workspace/cmake/project_lib/src/div.cpp" "CMakeFiles/prog.dir/src/div.cpp.o" "gcc" "CMakeFiles/prog.dir/src/div.cpp.o.d"
+  "/Users/lilithgames/Desktop/workspace/cmake/project_lib/src/mult.cpp" "CMakeFiles/prog.dir/src/mult.cpp.o" "gcc" "CMakeFiles/prog.dir/src/mult.cpp.o.d"
+  "/Users/lilithgames/Desktop/workspace/cmake/project_lib/src/sub.cpp" "CMakeFiles/prog.dir/src/sub.cpp.o" "gcc" "CMakeFiles/prog.dir/src/sub.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project_lib/build/CMakeFiles/prog.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "/Users/lilithgames/Desktop/workspace/cmake/project_lib/lib/libprog.dylib"
+  "/Users/lilithgames/Desktop/workspace/cmake/project_lib/lib/libprog.pdb"
+  "CMakeFiles/prog.dir/src/add.cpp.o"
+  "CMakeFiles/prog.dir/src/add.cpp.o.d"
+  "CMakeFiles/prog.dir/src/div.cpp.o"
+  "CMakeFiles/prog.dir/src/div.cpp.o.d"
+  "CMakeFiles/prog.dir/src/mult.cpp.o"
+  "CMakeFiles/prog.dir/src/mult.cpp.o.d"
+  "CMakeFiles/prog.dir/src/sub.cpp.o"
+  "CMakeFiles/prog.dir/src/sub.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
